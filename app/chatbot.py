e3bd19f0eeb1a358e85ec2cbecbc1df3ad692c15
priyankalisa <lisapriyank12@gmail.com>
2026-05-31 23:59:32 +0530

    fix Gemini API key for Render deployment

--- app/chatbot.py
+++ app/chatbot.py
@@ -1,9 +1,11 @@
 import streamlit as st
+import os
 import google.generativeai as genai
 
-genai.configure(
-    api_key=st.secrets["GEMINI_API_KEY"]
-)
+# Load API key safely (works locally + Render)
+api_key = os.getenv("GEMINI_API_KEY")
+
+genai.configure(api_key=api_key)
 
 model = genai.GenerativeModel("gemini-2.5-flash")
 
