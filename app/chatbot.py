0c5e5eec5fc2b77ed68c4bfca84268eb28845a01
priyankalisa <lisapriyank12@gmail.com>
2026-06-02 00:04:18 +0530

    update Gemini model

--- app/chatbot.py
+++ app/chatbot.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import google.generativeai as genai
 import time
-from app.cache import get_cached_response, set_cache
+from cache import get_cached_response, set_caches
 
 # =========================
 # API KEY CONFIG
